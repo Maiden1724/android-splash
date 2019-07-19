e07563fa4b81f48724a83a997756fcc70301a778
Maiden1724 <anniepartez879@gmail.com>
2019-07-19 10:19:50 +0300

    first commit

---
+++ app/src/main/java/com/ghost/htmlsplashapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.ghost.htmlsplashapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+    WebView pageyangu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        pageyangu = findViewById(R.id.webyangu);
+        WebSettings settings = pageyangu.getSettings();
+        settings.setJavaScriptEnabled(true);
+      // pageyangu.loadUrl("file:///android_asset/Home.HTML");
+        pageyangu .loadUrl("https://www.emobilis.ac.ke");
+    }
+}
